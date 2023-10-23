f66a3c24063c27c0874e95f305fa6beddf52935f
Daniel Landry <Daniel.Landry@sparkhound.com>
2023-10-23 09:56:16 -0500

    Initial test commit of 'dungeon' project I am building to learn C#'.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// 10/21/23 | Writing a barebones 'RPG' style console game to familarize myself more with C#.
+
+// TODO:
+    // Basic menu options to start game or exit.
+    // Create a PLAYER class/object that will hold one or two values that will increase as the game progresses.
+        // Values: EXPERIENCE, LEVEL, and ATTACK.
+    // Create a system that will track the EXPERIENCE value and increase the player LEVEL based on given thresholds.
+    // Create an NPC class/object that will be used against the PLAYER.
