c4b7784f80bbc5473ff250136bdbd8ad2aa7f932
Daniel Landry <daniel.landry@sparkhound.com>
2023-11-03 11:00:46 -0500

    Methods confirmed to work using static stats. Going to start adding randomization.

--- Program.cs
+++ Program.cs
@@ -9,32 +9,94 @@
     // Create a system that will track the EXPERIENCE value and increase the player LEVEL based on given thresholds.
     // Create an NPC class/object that will be used against the PLAYER.
 
-class Player
-{
-    public string Name = "Player";
-    public int Level, Experience;
-    public int HealthPoints = 5;
-    public int AttackPoints = 5;
-}
+using System;
 
-class NPC
+namespace DungeonGame
 {
-    public string Name;
-    public int Level, HealthPoints, AttackPoints;
-
-}
-
-class Dungeon
-{
-    // Method GenerateNPC to generate NPC stats to pair against the Player in the Battle method.
-    public static class NewNPC()
+    class Player
     {
+        public string Name = "Player";
+        public int Level = 1, Experience = 0, HealthPoints = 5, AttackPoints = 5;
+    }
+
+    class NPC
+    {
+        public string Name = "NPC";
+        public int Level = 1, HealthPoints = 1, AttackPoints = 5;
 
     }
 
-    // Method 'Battle' to perform the calculations against the Player and the randomly chosen NPC then return a victory or a loss.
-    public static string Battle(int HealthPoints, int AttackPoints)
+    class Dungeon
     {
-        if (HealthPoints > 0) { }
+        public bool victory = true;
+        // Method GenerateNPC to generate NPC stats to pair against the Player in the Battle method.
+
+        // Method 'Battle' to perform the calculations against the Player and the randomly chosen NPC then return a victory or a loss.
+        public static bool Battle(int playerAttack, int npcAttack, bool victory)
+        {
+            
+            Console.WriteLine($"Your AttackPoints: {playerAttack}\tNPC AttackPoints: {npcAttack}");
+            
+            if (playerAttack > npcAttack) 
+            {
+                victory = true;
+                Console.WriteLine("You won!");
+                Console.ReadLine();
+            }
+            else
+            {
+                victory = false;
+                Console.WriteLine("You lost!");
+                Console.ReadLine();
+            }
+            return victory;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            // bool start = false;
+
+            Console.WriteLine("Start Game? (Y/N)");
+            var startGame = Console.ReadLine();
+            if (startGame == "N")
+            {
+                Main();
+            }
+            else if (startGame == "Y")
+            {
+                Console.WriteLine("Starting...");
+            }
+            Console.WriteLine("Enter a name: ");
+            string submittedName = Console.ReadLine();
+            Console.WriteLine("Initializing new player...");
+
+            Player Player1 = new Player();
+            NPC Npc1 = new NPC();
+
+            // Player1.Name = submittedName;
+            // Player1.Name = submittedName;
+            Console.WriteLine($"Name: {Player1.Name}\nLevel: {Player1.Level}\nHealthPoints: {Player1.HealthPoints}\nAttackPoints: {Player1.AttackPoints}\nExperience: {Player1.Experience}\n\n");
+            
+            Console.WriteLine("Battle? (Y/N)");
+            var startBattle = Console.ReadLine();
+            
+            if (startBattle == "N")
+            {
+                Main();
+            }
+            else if (startBattle == "Y")
+            {
+                Dungeon Dungeon1 = new Dungeon();
+                while (Dungeon1.victory == true)
+                {
+                    Dungeon.Battle(Player1.AttackPoints, Npc1.AttackPoints, Dungeon1.victory);
+                }
+            }
+            
+            Console.ReadLine();
+        }
     }
 }
